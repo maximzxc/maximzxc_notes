ba058141b8b0b61a90c6fd71c235a85ea1e100cb
maximzxc <maximzxc@gmail.com>
2013-10-09 19:33:12 +0300

    added TB interface

--- notessite/urls.py
+++ notessite/urls.py
@@ -1,18 +1,15 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.conf.urls.static import static
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
+import views
+
 urlpatterns = patterns('',
-	(r'^notes/', include('notessite.apps.Notes.urls')),
-    # Examples:
-    # url(r'^$', 'notessite.views.home', name='home'),
-    # url(r'^notessite/', include('notessite.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
-)
+                       (r'^notes/', include('notessite.apps.Notes.urls')),
+                       url(r'^$', views.home, name='home'),
+                       (r'^admin/', include(admin.site.urls))) + static(
+settings.STATIC_URL, document_root=settings.STATIC_ROOT)
