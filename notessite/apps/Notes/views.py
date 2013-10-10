4093c3aba93e5fce1ff88ea66926ba6627e386cd
maximzxc <maximzxc@gmail.com>
2013-10-10 18:33:06 +0300

    add pagination

--- notessite/apps/Notes/views.py
+++ notessite/apps/Notes/views.py
@@ -7,5 +7,8 @@ class NotesListView(ListView):
 
     context_object_name = "notes_list"
     template_name = "notes/list.html"
-    queryset = Note.objects.order_by("added")
+    #queryset = Note.objects.order_by("added")
     paginate_by = 10
+
+    def get_queryset(self):
+            return Note.objects.order_by("added")
