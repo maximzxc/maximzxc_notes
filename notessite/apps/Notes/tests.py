b8e1487ae862eadab907d3b53e0c08dc384f2ab7
maximzxc <maximzxc@gmail.com>
2013-10-10 22:19:57 +0300

    add some tests

--- notessite/apps/Notes/tests.py
+++ notessite/apps/Notes/tests.py
@@ -7,6 +7,8 @@ Replace this with more appropriate tests for your application.
 
 from django.test import TestCase
 from django.core.urlresolvers import reverse
+from django_webtest import WebTest
+from models import Note
 
 
 class NotesViewsTestCase(TestCase):
@@ -17,4 +19,16 @@ class NotesViewsTestCase(TestCase):
         self.assertEqual(resp.status_code, 200)
         resp = self.client.get(reverse('home'))
         self.assertEqual(resp.status_code, 200)
-        self.assertTrue('notes_list' in resp.context)
+
+
+class ListsTest(WebTest):
+    fixtures = ['notes_views_testdata.json']
+
+    def test_list(self):
+        note1 = Note.objects.get(pk=1)
+        note2 = Note.objects.get(pk=2)
+        page = self.app.get(reverse('notes_list'))
+        assert note1.title in page
+        assert note2.title in page
+        assert note1.content in page
+        assert note2.content in page
