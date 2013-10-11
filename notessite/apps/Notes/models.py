18c1b7e3c807c829af0e803aade3651177aa26b0
maximzxc <maximzxc@gmail.com>
2013-10-11 16:26:01 +0300

    docstrig added

--- notessite/apps/Notes/models.py
+++ notessite/apps/Notes/models.py
@@ -4,6 +4,25 @@ from django.db import models
 
 
 class Note(models.Model):
+    '''
+    Every Note have title, content, date of adding and update date
+
+    title field created for Notes captions
+    content field created for text that they contains
+    added shows when Note was added to database
+    updated shows when Note was edited, if it wasn't edited it shows
+    same that 'added' field
+
+    #Create some notes
+    >>> note1 = Notes.objects.create(title='qwe', content='asdasd')
+    >>> note2 = Notes.objects.create(title='zxc', content='asd123asd')
+
+    #Show title of Note
+    >>>note1.title
+
+    #Show content of Note
+    >>>note2.content
+    '''
     title = models.CharField(max_length=255)
     content = models.TextField()
     added = models.DateTimeField(auto_now_add=True)
--- notessite/urls.py
+++ notessite/urls.py
@@ -1,16 +1,12 @@
 from django.conf.urls import patterns, include, url
-from django.conf import settings
-from django.conf.urls.static import static
-from views import home
+from notessite.apps.Notes.views import home
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
-import views
 
 urlpatterns = patterns('',
                        (r'^notes/', include('notessite.apps.Notes.urls')),
                        url(r'^$', home, name='home'),
                        (r'^admin/', include(admin.site.urls)))
-urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
